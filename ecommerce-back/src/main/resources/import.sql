d99b0951ae74ec554c9731336cf04f7e01d973cc
caiosena12 <caiosenadasilva@gmail.com>
2026-09-03 04:09:45 +0000

    Implementa entidades e dados iniciais

---
+++ ecommerce-back/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO categoria (nome, descricao) VALUES ('informatica', 'profutos de informatica');
+
+INSERT INTO categoria (nome, descricao) VALUES ('livros', 'livros tecnicos');
+
+INSERT INTO produtos (nome, descricao, preco, estoque, categoria_id) values('código limpo', 'livro do autor Robert C. Martin', 87.34);
+
